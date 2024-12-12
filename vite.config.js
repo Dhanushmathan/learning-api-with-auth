51a1fc66d525c66ffc144db13df6be92ec8d3d05
Dhanushmathan <lovelymathan2394@gmail.com>
2024-12-12 16:41:40 +0530

    fix: vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    base: '/learning-api-with-auth/',
+    // build: {
+    //   rollupOptions: {
+    //     input: {
+    //       // main: resolve(__dirname, "index.html"),
+    //     },
+    //   },
+    // },
+});
